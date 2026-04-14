e5258466dd0b1798bbcb082248102e8cbd2e5843
Fuad Sharif <fuadsharif004@gmail.com>
2026-04-15 00:36:51 +0600

    Add files via upload

--- task.py
+++ task.py
@@ -27,28 +27,32 @@ scissors = '''
 ---.__(___)
 '''
 
+
 game_images = [rock, paper, scissors]
 
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-# Note: it's worth checking if the user has made a valid choice before the next line of code.
-# If the user typed somthing other than 0, 1 or 2 the next line will give you an error.
-# You could for example write:
-if user_choice >= 0 and user_choice <= 2:
-    print(game_images[user_choice])
+my_choice= int(input("0 for Rock, 1 for Paper, 2 for Scissors: "))
+if my_choice >=0 and my_choice<=2:
+    print(game_images[my_choice])
 
-computer_choice = random.randint(0, 2)
-print("Computer chose:")
+computer_choice = random.randint(0,2)
+print(f"computer choice {computer_choice}")
 print(game_images[computer_choice])
 
-if user_choice >= 3 or user_choice < 0:
-    print("You typed an invalid number. You lose!")
-elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-    print("You lose!")
-elif computer_choice > user_choice:
-    print("You lose!")
-elif user_choice > computer_choice:
-    print("You win!")
-elif computer_choice == user_choice:
-    print("It's a draw!")
+if my_choice>=3 or my_choice<0:
+    print("Invalid choice,You lose")
+elif my_choice == 0 and computer_choice == 1:
+    print("computer wins")
+elif my_choice ==0 and computer_choice == 2:
+    print("I win")
+elif my_choice ==1 and computer_choice == 0:
+    print("I win")
+elif my_choice ==1 and computer_choice ==2:
+    print("Computer wins")
+elif my_choice ==2 and computer_choice == 0:
+    print("Computer wins")
+elif my_choice ==2 and computer_choice==1:
+    print("I win")
+elif my_choice == computer_choice:
+    print("Draw")
+
+
